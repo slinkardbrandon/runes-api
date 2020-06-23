bece90749df61534b927345ed752effc392ce1eb
Brandon Slinkard <slinkardbrandon@gmail.com>
2020-06-22 21:00:45 -0500

    Adding elder futhark set

---
+++ src/enums/Aett.ts
@@ -0,0 +1,5 @@
+export enum Aett {
+  Freya = 'freya',
+  Heimdall = 'heimdall',
+  Tiwaz = 'tiwaz',
+}
---
+++ src/interfaces/IRune.ts
@@ -0,0 +1,7 @@
+import { Aett } from '../enums/Aett';
+
+export interface IRune {
+  name: string;
+  imageUrl: string;
+  aett: Aett;
+}
